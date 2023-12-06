c161755b909d6b39f0822b79af593ea7a71bf4a3
Afonso Barreto <fernando.analista.si@gmail.com>
2023-12-06 11:42:30 -0300

    Creating destructible cover

--- scripts/object.gd
+++ scripts/object.gd
@@ -2,6 +2,8 @@ extends RigidBody2D
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity") * -1
 var gravity_vector: Vector2 = Vector2(0, 0)
+var previous_velocity: float
+var actual_velocity: float
 
 func _ready():
 	GravityInformation.connect("up_direction_change", change_up_direction)
@@ -11,6 +13,13 @@ func _ready():
 
 func _physics_process(_delta):
 	apply_central_force(gravity_vector)
+	
+func _integrate_forces(state):
+	previous_velocity = actual_velocity
+	actual_velocity = state.linear_velocity.length()
+	
+func get_velocity():
+	return abs(previous_velocity - actual_velocity)
 
 ## Change gravity direction upon signal
 func change_up_direction(upDirection: Vector2):
