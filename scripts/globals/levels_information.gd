7f3c79514dba79d584bdce14837d7294348a75df
Harry <fmjh@hotmail.com>
2023-12-11 17:06:44 -0300

    Adding level to present the whormwhole mechanic

--- scripts/globals/levels_information.gd
+++ scripts/globals/levels_information.gd
@@ -16,7 +16,8 @@ var levels : Array[String] = [
 	"res://scenes/levels/pres_field.tscn",
 	"res://scenes/levels/ref_field.tscn",
 	"res://scenes/levels/pres_cover.tscn",
-	"res://scenes/levels/ref_cover.tscn"
+	"res://scenes/levels/ref_cover.tscn",
+	"res://scenes/levels/pres_wormhole.tscn"
 ]
 var current_level: int = 0
 
