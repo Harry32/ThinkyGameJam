22189ce88f6ac2292103a255c733d333b33e6bcd
Gabriel Arimatéa <gabriel.arimatea@gmail.com>
2023-12-11 15:57:11 -0300

    feat/#14: Finishing CI/CD Pipeline!

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,19 @@
 
 set -e
 
-/usr/local/bin/godot --headless --export-$1 "Web" ../build/index.html
-cd ../build
+echo "Dealing with templates"
+
+mkdir /build
+mkdir -p /github/home/.local/share/godot/export_templates/${GODOT_VERSION}.${RELEASE_NAME}
+wget -q https://github.com/godotengine/godot/releases/download/${GODOT_VERSION}-${RELEASE_NAME}/Godot_v${GODOT_VERSION}-${RELEASE_NAME}_export_templates.tpz
+unzip -q Godot_v${GODOT_VERSION}-${RELEASE_NAME}_export_templates.tpz
+mv templates/*  /github/home/.local/share/godot/export_templates/${GODOT_VERSION}.${RELEASE_NAME}
+rm -f Godot_v${GODOT_VERSION}-${RELEASE_NAME}_export_templates.tpz Godot_v${GODOT_VERSION}-${RELEASE_NAME}_${GODOT_PLATFORM}.zip
+
+echo "Starting Building"
+/usr/local/bin/godot --headless --path /game -q --export-release Web /build/index.html
+
+echo "Zipping and Pushing"
+cd /build
 zip -r build.zip .
 /opt/butler/bin/butler push build.zip harry32/tgjgravgame:html5
